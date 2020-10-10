5e631a6a844bad1152fb8eec8c49c49f8356e116
Fuxing Loh <git@fuxing.dev>
2020-10-10 18:47:11 +0800

    Changed max file size limit, fixed learn content routing

---
+++ web/pages/learn/_group/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <learn-page-parent-child v-if="page.layered" :page="page"/>
+  <learn-page v-else :page="page"/>
+</template>
+
+<script>
+import LearnPage from "@/components/learn/LearnPage";
+import LearnPageParentChild from "@/components/learn/LearnPageLayered";
+
+export default {
+  components: {LearnPageParentChild, LearnPage},
+  async asyncData({$content, params: {group, slug}}) {
+    const page = await $content(`learn/${group}/${slug}`, {text: true}).fetch()
+    return {page}
+  }
+}
+</script>
